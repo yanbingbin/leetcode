3613026b0afeb7b5c5b4f51b71765b69904d50d0
yanbingbin <785175917@qq.com>
2020-06-06 23:31:43 +0800

    Create 128最长连续子序列.md

--- tpl/test.js
+++ tpl/test.js
@@ -1,22 +1,21 @@
-var longestPalindrome = function(s) {
-    const len = s.length;
-    if (len < 2) return s;
-
-    let dp = Array.from(new Array(len), () => new Array(len).fill(false));
-    let res = '';
-
-    // 第一层倒着循环，才能保证 dp[i+1][j-1] 已经存在
-    for (let i = len - 1; i >= 0; i--) {
-        for (let j = i; j < len; j++) {
-             // 判断i 和 j下标的字符串相等时
-            //如果间隔小于等于2，则代表length为 3以内的子字符串，则一定是回文子串
-            //如果间隔 大于2时，则需要判断 dp[i+1][j-1] 是否为回文子串
-            dp[i][j] = s.charAt(i) === s.charAt(j) && (j - i <= 2 || dp[i+1][j-1])
-            // 判断符合回文的最大子字符串
-            if(dp[i][j] && j - i >= res.length){
-                res = s.slice(i,j+1)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    let uniqueNums = new Set(nums);
+    let count = 1;
+    let maxCount = 1;
+    for (let i = 0; i < nums.length; i++) {
+        if (!uniqueNums.has(nums[i] - 1)) { // 右边没值说明是起点
+            let cur = nums[i];
+            while (uniqueNums.has(cur + 1)) {
+                cur++;
+                count++;
             }
-        }
+            maxCount = Math.max(maxCount, count);
+        } 
+        count = 1;
     }
-    return res;
+    return maxCount;
 };
