e99fb6f717ce9befd8392a04796537397a58de22
颜秉斌 <785175917@qq.com>
2021-02-28 20:21:13 +0800

    test: 测试github访问

--- tpl/test.js
+++ tpl/test.js
@@ -1,21 +1,8 @@
 /**
- * @param {number[]} nums
+ * @param {string} s
+ * @param {number} k
  * @return {number}
  */
-var longestConsecutive = function(nums) {
-    let uniqueNums = new Set(nums);
-    let count = 1;
-    let maxCount = 1;
-    for (let i = 0; i < nums.length; i++) {
-        if (!uniqueNums.has(nums[i] - 1)) { // 右边没值说明是起点
-            let cur = nums[i];
-            while (uniqueNums.has(cur + 1)) {
-                cur++;
-                count++;
-            }
-            maxCount = Math.max(maxCount, count);
-        } 
-        count = 1;
-    }
-    return maxCount;
+var longestSubstring = function(s, k) {
+    
 };
